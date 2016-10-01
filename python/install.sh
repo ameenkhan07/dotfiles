11e71e11c68991ec4f993acbc8096b4f553a5485
ameenkhan07 <ameenkhan07@gmail.com>
2016-10-01 12:02:07 +0530

    pip modules added

--- python/install.sh
+++ python/install.sh
@@ -1,12 +1,21 @@
 # Python Stuff
 sudo apt-get install python
 sudo apt-get install python3
+sudo apt-get install python-dev python3-dev
+
+# IPYTHON
+sudo apt-get install ipython
+
+# Jupyter Notebook
+pip3 install jupyter
 
 ## PIP Virtualenv Installs
 wget -O ~/Downloads/get_pip.py https://bootstrap.pypa.io/get-pip.py
 sudo python ./Downloads/get_pip.py
 sudo pip install virtualenv
 rm -rf ~/Downloads/get_pip.py
+
+sudo apt install python3-pip
 ## Niche Pythons
 sudo pip install sniffer #Continuous testing on python
 sudo pip install cheat # Cheatsheets on commands
@@ -15,3 +24,4 @@ sudo pip install cheat # Cheatsheets on commands
 wget -O ~/Downloads/anaconda.sh http://repo.continuum.io/archive/Anaconda3-4.1.1-Linux-x86_64.sh
 bash ~/Downloads/anaconda.sh
 rm -rf ~/Downloads/anaconda.sh
+
