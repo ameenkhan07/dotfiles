e99c0bf8cc23965b34a7b0105c7e7b24911cc4e8
ameenkhan07 <ameenkhan07@gmail.com>
2016-10-08 22:13:26 +0530

    The FUCK : https://github.com/nvbn/thefuck

--- python/install.sh
+++ python/install.sh
@@ -1,13 +1,17 @@
 # Python Stuff
 sudo apt-get install python
 sudo apt-get install python3
-sudo apt-get install python-dev python3-dev
+sudo apt-get install python-dev python3-dev python3-pip
 
 # IPYTHON
 sudo apt-get install ipython
 
 # Jupyter Notebook
 pip3 install jupyter
+
+# The Fuck
+# https://github.com/nvbn/thefuck
+pip3 install --user thefuck
 
 ## PIP Virtualenv Installs
 wget -O ~/Downloads/get_pip.py https://bootstrap.pypa.io/get-pip.py
