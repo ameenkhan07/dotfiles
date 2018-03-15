7f9961f242114937df027e34bd6269582260972a
ameenkhna07 <ameenkhan07@gmail.com>
2018-03-15 12:54:30 +0530

    minor changes, oldy

--- scala/install.sh
+++ scala/install.sh
@@ -1,19 +1,14 @@
-# Install JAVA 
-sudo apt-get install python-software-properties
-sudo add-apt-repository ppa:webupd8team/java
-sudo apt-get update
-sudo apt-get install oracle-java8-installer
-
-# Installing Scala
-wget -O ~/Downloads/scala/scala.deb http://www.scala-lang.org/files/archive/scala-2.11.6.deb
-sudo dpkg -i ~/Downloads/scala/scala.deb
-sudo apt-get update && sudo apt-get install scala
-rm -rf ~/Downloads/scala/scala.deb
-# Installing SBT
-echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 642AC823
-sudo apt-get update && sudo apt-get install sbt
-# Installing Conscript, Giter8
-curl https://raw.githubusercontent.com/foundweekends/conscript/master/setup.sh | sh # export PATH=$PATH:~/.conscript/bin
-cs foundweekends/giter8
+# 
+# # Installing Scala
+# wget -O ~/Downloads/scala/scala.deb http://www.scala-lang.org/files/archive/scala-2.11.6.deb
+# sudo dpkg -i ~/Downloads/scala/scala.deb
+# sudo apt-get update && sudo apt-get install scala
+# rm -rf ~/Downloads/scala/scala.deb
+# # Installing SBT
+# echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+# sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 642AC823
+# sudo apt-get update && sudo apt-get install sbt
+# # Installing Conscript, Giter8
+# curl https://raw.githubusercontent.com/foundweekends/conscript/master/setup.sh | sh # export PATH=$PATH:~/.conscript/bin
+# cs foundweekends/giter8
 
