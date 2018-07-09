3628c4d8fe5f4de348433b77707b874bcc203470
ameenkhna07 <ameenkhan07@gmail.com>
2018-07-09 15:03:10 +0530

    Added python 3 pip

--- python/install.sh
+++ python/install.sh
@@ -1,5 +1,7 @@
 # # Extra
 sudo pip install httpie
-
+sudo pip3 install ipython
+sudo pip install pipenv
+sudo pip3 install pipenvpip
 # For lack of a better place, ssh enable here
 sudo systemsetup -setremotelogin on
