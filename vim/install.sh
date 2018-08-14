989a12eca65563c6f6679ccc58edd2af07ebb27e
ameenkhan07 <ameenkhan07@gmail.com>
2018-08-13 21:34:06 -0400

    Cleanup and typo's corrected

--- vim/install.sh
+++ vim/install.sh
@@ -1,7 +1,2 @@
 # Install Vim Package manager for good measure
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
-
-### Pre Reqs for YouCompleteMe Plugin
-# cd ~/.vim/bundle/YouCompleteMe; git submodule update --init --recursive  # Incase Vundle screws up
-cd ~/.vim/bundle/YouCompleteMe;./install.py --all
